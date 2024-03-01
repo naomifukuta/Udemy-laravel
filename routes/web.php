e1cd5b3a527d23a88811e94a4ac28d142ad722af
naomifukuta <itismenaomi09@gmail.com>
2024-03-01 22:56:29 +0900

    constraining the route parameter format

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,10 @@ Route::get('/contacts/create',function(){
 
 Route::get('/contacts/{id}',function($id){
     return "Contact".$id;
-});
+})->whereNumber('id');
+//where('id','[0-9]+1'); //set only numbers //if you input /contacts/john  you get an error.
+
+
 
 Route::get('/companies/{name?}', function($name=null){
     if($name){
@@ -36,4 +39,14 @@ Route::get('/companies/{name?}', function($name=null){
     }else {
         return "All companies";
     }
-});
+})->whereAlpha('name');
+//('name','[a-zA-Z]+'); //set only letters
+
+
+Route::get('/bars/{name?}', function($name=null){
+    if($name){
+        return "company" . $name;
+    }else {
+        return "All companies";
+    }
+})->whereAlphaNumeric('name');
