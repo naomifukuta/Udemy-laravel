1329f24e7f4d7460162dfee811491fe525f441a0
naomifukuta <itismenaomi09@gmail.com>
2024-03-01 22:41:48 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route ::get('/contacts',function(){
+    return"<h1>All contacts</h1>";
+});
+
+Route::get('/contacts/create',function(){
+    return "<h1>add new contact </h1>";
+});
+
+Route::get('/contacts/{id}',function($id){
+    return "Contact".$id;
+});
+
+Route::get('/companies/{name?}', function($name=null){
+    if($name){
+        return "company" . $name;
+    }else {
+        return "All companies";
+    }
+});
