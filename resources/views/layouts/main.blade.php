a809b5ee9d933ac4bc9c63536f1a748d4198a7b9
naomifukuta <itismenaomi09@gmail.com>
2024-03-04 22:02:05 +0900

    blade template inheritance

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,8 +10,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Varela+Round">
     <!-- Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="{{ asset ('assets/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset ('assets/css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset ('/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset ('/css/custom.css') }}" rel="stylesheet">
   </head>
   <body>
     <!-- navbar -->
@@ -48,7 +48,7 @@
     </nav>
 {{-- content --}}
     
-
+@yield('content')
 
 
 
