4fa31de3dc5c6f46b07252f74daa81e5295de476
naomifukuta <itismenaomi09@gmail.com>
2024-03-05 21:38:08 +0900

    from routes to controllers

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $companies = [
+            1 => ['name' => 'Company One', 'contacts' => '3'],
+            2 => ['name' => 'Company two', 'contacts' => '5'],
+        ];
+    
+        $contacts = $this->getContacts();
+        return view('contacts.index',compact('contacts','companies'));
+    }
+
+
+    protected function getContacts(){
+        return[
+            
+                1 => ['id'=>1,'name' => 'Name 1', 'phone' => '1234567890'],
+                2 => ['id'=>2,'name' => 'Name 2', 'phone' => '2345678901'],
+                3 => ['id'=>3,'name' => 'Name 3', 'phone' => '3456789012'],
+            ];
+        
+    }
+
+    protected function create()
+    {
+        return view('contacts.create');
+    }
+
+    protected function show($id){
+        $contacts =$this-> getContacts();
+        abort_unless(isset($contacts[$id]),404);
+        $contact = $contacts[$id];
+        return view('contacts.show')->with('contact',$contact);
+    
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,42 +13,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-function getContacts(){
-    return[
-        
-            1 => ['id'=>1,'name' => 'Name 1', 'phone' => '1234567890'],
-            2 => ['id'=>2,'name' => 'Name 2', 'phone' => '2345678901'],
-            3 => ['id'=>3,'name' => 'Name 3', 'phone' => '3456789012'],
-        ];
-    
-}
+
 Route::get('/', function () {
   
     return view('welcome');
 });
 
 
-Route ::get('/contacts',function(){
-    $companies = [
-        1 => ['name' => 'Company One', 'contacts' => '3'],
-        2 => ['name' => 'Company two', 'contacts' => '5'],
-    ];
+Route ::get('/contacts',[ContactController::class ,'index'])->name('contacts.index');
 
-    $contacts = getContacts();
-    return view('contacts.index',compact('contacts','companies'));
-})->name('contacts.index');
+Route::get('/contacts/create',[ContactController::class,'create'])->name('contacts.create');
 
-Route::get('/contacts/create',function(){
-    return view('contacts.create');
-})->name('contacts.create');
-
-Route::get('/contacts/{id}',function($id){
-    $contacts = getContacts();
-    abort_unless(isset($contacts[$id]),404);
-    $contact = $contacts[$id];
-    return view('contacts.show')->with('contact',$contact);
-    
-})->name('contacts.show');
+Route::get('/contacts/{id}',[ContactController::class,'show'])->name('contacts.show');
 //where('id','[0-9]+1'); //set only numbers //if you input /contacts/john  you get an error.
 
 
