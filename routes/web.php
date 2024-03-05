4ab87d656a06e5eeb354ba43b989c5c855ae2eeb
naomifukuta <itismenaomi09@gmail.com>
2024-03-05 21:46:26 +0900

    grouping the controller routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::controller(ContactController::class)->name('contacts.')->group(function(){
 
-Route ::get('/contacts',[ContactController::class ,'index'])->name('contacts.index');
+    Route ::get('/contacts','index')->name('index');
 
-Route::get('/contacts/create',[ContactController::class,'create'])->name('contacts.create');
+    Route::get('/contacts/create','create')->name('create');
+    
+    Route::get('/contacts/{id}', 'show')->name('show');
 
-Route::get('/contacts/{id}',[ContactController::class,'show'])->name('contacts.show');
+
+
+});
+
 //where('id','[0-9]+1'); //set only numbers //if you input /contacts/john  you get an error.
 
 
